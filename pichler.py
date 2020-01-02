8c185314c296fa56625def55e847a049d8893e73
ge0rdi <ge0rdi@users.noreply.github.com>
2020-01-02 21:21:51 +0100

    Datapoint definitions
    To simplify value reading.
    
    Caller doesn't need to know exact addresses of data points nor how to
    interpret raw data.

--- pichler.py
+++ pichler.py
@@ -2,6 +2,7 @@ import sys
 import os
 import nabto
 import json
+from collections import namedtuple
 try:
 	import configparser
 except:
@@ -12,6 +13,41 @@ except:
 package_dir = os.path.dirname(os.path.abspath(__file__))
 
 class Pichler:
+	# datapoint definitions
+	DPItem = namedtuple('DPItem', ['addr', 'scale'])
+	DP = {
+		'StatusBits': DPItem((152, 1), 1),
+		'Malfunction': DPItem((34, 0), 1),
+		'CO2': DPItem((153, 1), 1),
+		'Humidity': DPItem((154, 1), 1),
+		'SCOP': DPItem((44, 0), 0.01),
+		'HP.COP': DPItem((45, 0), 0.01),
+		'HP.HeatingPower': DPItem((37, 0), 1),
+
+		'Energy.Total': DPItem((65, 0), 1),
+		'Energy.Heating': DPItem((64, 0), 1),
+		'Energy.Cooling': DPItem((27, 0), 1),
+		'Energy.HotWater': DPItem((38, 0), 1),
+		'Energy.Ventilation': DPItem((29, 0), 1),
+
+		'Power.HeatPump': DPItem((25, 0), 0.1),
+		'Power.HotWater': DPItem((24, 0), 0.1),
+		'Power.Ventilation': DPItem((26, 0), 0.1),
+
+		'Temperature.Room': DPItem((19, 0), 0.01),
+
+		'Temperature.Air.Supply': DPItem((1, 1), 0.01),
+		'Temperature.Air.Extract': DPItem((6, 1), 0.01),
+		'Temperature.Air.Outdoor': DPItem((2, 1), 0.01),
+		'Temperature.Air.Exhaust': DPItem((3, 1), 0.01),
+
+		'Temperature.Water.Center': DPItem((12, 1), 0.01),
+		'Temperature.Water.Bottom': DPItem((13, 1), 0.01),
+
+		'Ventilation.Level': DPItem((41, 1), 1),
+		'Ventilation.Supply': DPItem((22, 0), 0.1),
+		'Ventilation.Extract': DPItem((23, 0), 0.1),
+	}
 
 	def __init__(self, device=None, user=None, passwd=None):
 		if not device or not user or not passwd:
@@ -36,6 +72,27 @@ class Pichler:
 		    rpc_xml = file.read()
 
 		self.session.RpcSetDefaultInterface(rpc_xml)
+
+	# Get value of single datapoint
+	# dp: datapoint name (see `DP` dict)
+	# output: real value of given datapoint (scaled appropriately)
+	def GetDatapoint(self, dp):
+		item = self.DP[dp]
+		return self.DatapointReadValue(item.addr[0], item.addr[1]) * item.scale
+
+	# Get values of multiple datapoints
+	# dps: list of datapoint names (see `DP` dict)
+	# output: list of real values of given datapoints (same order as input list)
+	def GetDatapoints(self, dps):
+		l = []
+		for dp in dps:
+			item = self.DP[dp]
+			l.append([item.addr[0], item.addr[1]])
+		r = self.DatapointReadListValues(l)
+		for i in range(len(dps)):
+			item = self.DP[dps[i]]
+			r[i] = r[i] * item.scale
+		return r
 
 	def RpcInvoke(self, command, params):
 		r = self.session.RpcInvoke('nabto://%s/%s.json?%s' % (self.device, command, params))
