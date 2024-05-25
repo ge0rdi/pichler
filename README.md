ca3711ec9d6c788b40525a015dfc8c863f4a3ba8
ge0rdi <ge0rdi@gmx.com>
2024-05-25 17:46:20 +0200

    Report values to MQTT

--- README.md
+++ README.md
@@ -3,24 +3,25 @@ Scripts for gathering data from [Pichler PKOM4](http://www.pichlerluft.at/heat-p
 
 ## Setup
 * Clone repo.
-* Download [Nabto](https://downloads.nabto.com/assets/nabto-libs/4.3.0/nabto-libs.zip) libraries.
+* Install dependencies (if you want to collect data):  
+  `python -m pip install -r requirements.txt`
+* Download [Nabto](https://downloads.nabto.com/assets/nabto-libs/4.4.0/nabto-libs.zip) libraries.
 * Unpack libraries (.dll, .so) for your OS to `libs` folder.
-* Provide device ID and credentials in `pichler.ini` file.
-  * To prevent accidental commit of your credentials, use:  
-  `git update-index --skip-worktree pichler.ini`
+* Set device id and user name/password using environment variables:
+  * `PICHLER_DEVICE_ID`
+  * `PICHLER_USER`
+  * `PICHLER_PASSWORD`
+* Set MQTT host/port using environment variables:
+  * `MQTT_HOST`
+  * `MQTT_PORT`
 * Test your setup by running `python info.py`.  
   It should connect to your device and output basic runtime values.
-* If you want to collect unit's data to Influx database you should also:
-  * Install [Influxdb](https://www.influxdata.com/blog/getting-started-python-influxdb/) python module:  
-  `python -m pip install influxdb`
-  * Provide Influx database setup in `collect.ini` file.
 
 ## Collecting data
-You can use `collect.py` script for periodic data collection on background. Script reads selected values (`data_points` array) from Pichler unit and stores them to [Infux](https://www.influxdata.com) database.
+You can use `collect.py` script for periodic data collection on background.  
+Script reads selected values (`data_points` array) from Pichler unit and publishes them to MQTT broker.
 
-Data will be read every minute and stored to db only when changed since last reading (or once an hour).
-
-`Influx` database setup (host, credentials, database name) is taken from `collect.ini` file.
+Data will be read and published every minute.
 
 To run `collect.py` in background use:  
 `python -u collect.py > collect.log 2>&1 &`
--- collect.py
+++ collect.py
@@ -1,146 +1,162 @@
 """
-Periodically read selected data from heat pump unit and store them to Influx database.
-
-Read data is stored to DB:
-* when its value has changed since previous reading
-* every hour
-
-InfluxDb config is taken from `collect.ini` file.
+Periodically read selected data from heat pump unit
 """
 
 import os
 import pichler
 import time
-from influxdb import InfluxDBClient
-try:
-	import configparser
-except:
-	# python2 compatibility
-	import ConfigParser
-	configparser = ConfigParser
+import traceback
+import paho.mqtt.client as mqtt
+
+debug = False
+
+MQTT_PREFIX = 'pkom4/'
 
 def Log(s):
 	print('[%s] %s' % (time.ctime(time.time()), s))
+def DebugLog(s):
+	if debug:
+		Log(s)
+
+set_points = [
+	('setting/water/temperature', 'HotWater.Temperature'),
+	('setting/water/e-heating', 'HotWater.E.Heating'),
+	('setting/water/fast-heating', 'HotWater.Fast.Heating'),
+	('setting/ventilation/temperature/normal', 'Temperature.Normal'),
+	('setting/ventilation/temperature/cooling', 'Temperature.ActiveCooling'),
+	('setting/operating-mode', 'OperatingMode'),
+]
 
 # datapoints to read
 data_points = [
-	'Energy.Total',
-	'Energy.Cooling',
-	'Energy.Heating',
-	'Energy.HotWater',
-	'Energy.Ventilation',
+	('energy', 'Energy.Total'),
+	('energy/cooling', 'Energy.Cooling'),
+	('energy/heating', 'Energy.Heating'),
+	('energy/water', 'Energy.HotWater'),
+	('energy/ventilation', 'Energy.Ventilation'),
 
-	'Power.HotWater',
-	'Power.HeatPump',
-	'Power.Ventilation',
+#	('power', ''),  # sum of items below
+	('power/heat-pump', 'Power.HeatPump'),
+	('power/water', 'Power.HotWater'),
+	('power/ventilation', 'Power.Ventilation'),
 
-	'Temperature.Room',
-	'Temperature.Air.Supply',
-	'Temperature.Air.Extract',
-	'Temperature.Air.Outdoor',
-	'Temperature.Air.Exhaust',
-	'Temperature.Water.Center',
-	'Temperature.Water.Bottom',
+	('water/temperature/center', 'Temperature.Water.Center'),
+	('water/temperature/bottom', 'Temperature.Water.Bottom'),
 
-	'Ventilation.Level',
-	'Ventilation.Supply',
-	'Ventilation.Extract',
+	('ventilation/temperature/room', 'Temperature.Room'),
+	('ventilation/temperature/supply', 'Temperature.Air.Supply'),
+	('ventilation/temperature/extract', 'Temperature.Air.Extract'),
+	('ventilation/temperature/outdoor', 'Temperature.Air.Outdoor'),
+	('ventilation/temperature/exhaust', 'Temperature.Air.Exhaust'),
+	('ventilation/level', 'Ventilation.Level'),
+	('ventilation/volume/supply', 'Ventilation.Supply'),
+	('ventilation/volume/extract', 'Ventilation.Extract'),
 
-	'StatusBits',
-	'CO2',
+	('co2', 'CO2'),
+	('status', 'StatusBits'),  # split into status/xyz bits below
 ]
 
 # `StatusBits` will be stored as individual bits rather than raw value
 status_bits = [
 	{
-		'mask': 1,
-		'name': 'Status.HotWater',
+		'mask': 0x0001,
+		'name': 'status/water',
 	},
 	{
-		'mask': 4,
-		'name': 'Status.Heating',
+		'mask': 0x0004,
+		'name': 'status/heating',
+	},
+	{
+		'mask': 0x0008,
+		'name': 'status/cooling',
+	},
+	{
+		'mask': 0x0200,
+		'name': 'status/fast-heating',
 	}
 ]
-
-Log('Started collecting data')
-
-package_dir = os.path.dirname(os.path.abspath(__file__))
-
-config = configparser.ConfigParser()
-config.read(os.path.join(package_dir, 'collect.ini'))
-
-# Influx database client
-influx = InfluxDBClient(config.get('influxdb', 'host'), config.getint('influxdb', 'port'), config.get('influxdb', 'username'), config.get('influxdb', 'password'), config.get('influxdb', 'database'))
 
 # Pichler device client
 device = pichler.Pichler()
 
-prev = [0] * len(data_points)
-power_total_prev = 0
-minutes = 0
+def on_connect(client, userdata, flags, reason_code, properties):
+	DebugLog(f"on_connect: {reason_code}, {flags}")
+	for m in set_points:
+		client.subscribe(MQTT_PREFIX + m[0] + '/set')
 
-data = []
+def on_message(client, userdata, msg):
+	DebugLog(f"on_message: {msg.topic}")
+	for m in set_points:
+		if msg.topic == (MQTT_PREFIX + m[0] + '/set'):
+			try:
+				Log(msg.topic + " " + msg.payload.decode())
+				if not debug:
+					device.SetSetpoint(m[1], float(msg.payload))
+			except Exception as e:
+				Log('Error in SetSetpoint: ' + str(e))
+				traceback.print_exc()
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.on_message = on_message
+
+mqtt_host = os.environ['MQTT_HOST']
+mqtt_port = os.environ.get('MQTT_PORT', '1883')
+
+Log("Connecting to MQTT")
 
 while True:
-	current_time = int(time.time())
+	try:
+		client.connect(mqtt_host, int(mqtt_port), 60)
+		break
+	except Exception as e:
+		Log('MQTT connect: ' + str(e))
+	time.sleep(10)
 
-	# read actual values
-	values = device.GetDatapoints(data_points)
+client.loop_start()
 
-#	print(values)
+Log('Collecting data')
 
-	# select values to report
-	report = [False] * len(data_points)
+while True:
 
-	for i in range(len(data_points)):
-		# value should be reported if it was changed or timeout exceeded
-		if (values[i] != prev[i]) or ((minutes % 60) == 0):
-			report[i] = True
+	try:
+		def Publish(name, value):
+			if not debug:
+				client.publish(MQTT_PREFIX + name, value)
+			else:
+				DebugLog(f"publish: {name} = {value}")
 
-	def AddMeasurement(name, value):
-		m = {'measurement': name.replace('.', '_'), 'time': current_time, 'fields': {'value': value}}
-		data.append(m)
+		# publish data to MQTT
+		for m in set_points:
+			Publish(m[0], device.GetSetpoint(m[1]))
 
-	# report values
-	for i in range(len(data_points)):
-		if report[i]:
+		# read actual values
+		values = device.GetDatapoints([d[1] for d in data_points])
+
+		# report values
+		for i in range(len(data_points)):
 			val = values[i]
 
 			# special handling for StatusBits
 			# report each bit separately
-			if data_points[i] == 'StatusBits':
+			if data_points[i][0] == 'status':
 				for d in status_bits:
-					val = 1 if (values[i] & d['mask']) != 0 else 0
-					AddMeasurement(d['name'], val)
-				continue
+					bval = 1 if (values[i] & d['mask']) != 0 else 0
+					Publish(d['name'], bval)
 
-			AddMeasurement(data_points[i], val)
+			Publish(data_points[i][0], val)
 
-	# get total power consumption (accumulate all power comsumption values)
-	power_total = 0
-	for i in range(len(data_points)):
-		if data_points[i].startswith('Power.'):
-			power_total += values[i]
+		# get total power consumption (accumulate all power comsumption values)
+		power_total = 0
+		for i in range(len(data_points)):
+			if data_points[i][0].startswith('power/'):
+				power_total += values[i]
 
-	# report if changed
-	if power_total != power_total_prev:
-		AddMeasurement('Power.Total', power_total)
+		Publish('power', power_total)
 
-	if data:
-#		print(data)
-		try:
-			# try to write data to database
-			# empty data on success (we don't need them anymore)
-			# otherwise keep them, so we'll try to write them on next occasion
-			if influx.write_points(data, time_precision='s'):
-				data = []
-		except Exception as e:
-			Log('Unexpected error: ' + str(e))
-
-	# store previous values
-	prev = values
-	power_total_prev = power_total
+	except:
+		Log('Unexpected error:')
+		traceback.print_exc()
 
 	# wait a minute
 	time.sleep(60)
-	minutes += 1
